5500b2d1e2c539e1f4af96d3facf834b688e4f9c
SHAIKH_ASRA_SWALEH <160813649+ammabb@users.noreply.github.com>
2024-03-03 14:27:39 +0530

    Update code.cpp
    1)Created reading the file
    2)Extracting the opcode and identifying if its R instruction or not
    3)Created a R.h file for R _format function ,it is added to same project

--- code.cpp
+++ code.cpp
@@ -1,1 +1,82 @@
 
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<fstream>
+#include "R.h"
+using namespace std;
+//R format
+vector<string>Rf;
+//I format
+
+int main()
+{
+    //Opening the assembly file
+    ifstream file("asembly_code.asm");
+    if (!file.is_open()) {
+        cout << "Error opening file." << endl;
+        return 0;
+    }
+    //Taking every line of assembly code as strings in vector
+    vector<string>lines;
+    string s;
+    while (getline(file, s)) {
+        lines.push_back(s);
+    }
+    //Completed taking file as input
+    file.close();
+    //Adding R format instructions
+    Rf.push_back("add");
+    Rf.push_back("and");
+    Rf.push_back("or");
+    Rf.push_back("sll");
+    Rf.push_back("slt");
+    Rf.push_back("sra");
+    Rf.push_back("srl");
+    Rf.push_back("sub");
+    Rf.push_back("xor");
+    Rf.push_back("mul");
+    Rf.push_back("div");
+    Rf.push_back("rem");
+    
+    int flag=0;
+    for(int i=0;i<lines.size();i++)
+    {
+        string ans="";
+        string opcode="";
+        int j=0;
+        //Getting the opcode until space and avoiding space in from of the opcode
+        while(lines[i][j]!=' ' || opcode=="")
+        {
+            //Checking if the instruction is comment or not
+            if(lines[i][j]=='#')
+            {
+                flag++;
+                break;
+            }
+            opcode=opcode+lines[i][j];
+            j++;
+            
+        }
+        
+        //If its a comment
+        if(flag)
+        {
+            flag=0;
+            continue;
+        }
+        //If not a comment then proceeding with instruction
+        for(int k=0;k<Rf.size();k++)
+        {
+            if(opcode==Rf[k])
+            {
+                ans=R(lines[i],j,opcode);
+                break;
+            }
+        }
+        
+
+    }
+    
+
+
+}
