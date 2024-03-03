c4f14317df0aff417e735fc11179e518ec265df4
SHAIKH_ASRA_SWALEH <160813649+ammabb@users.noreply.github.com>
2024-03-03 20:24:21 +0530

    Update code.cpp
    Addes I format ,S format Mnemonics

--- code.cpp
+++ code.cpp
@@ -1,12 +1,16 @@
-
 #include<bits/stdc++.h>
 #include<stdlib.h>
 #include<fstream>
 #include "R.h"
+#include "I.h"
+#include "S.h"
 using namespace std;
 //R format
 vector<string>Rf;
 //I format
+vector<string>If;
+//S format
+vector<string>Sf;
 
 int main()
 {
@@ -37,15 +41,32 @@ int main()
     Rf.push_back("mul");
     Rf.push_back("div");
     Rf.push_back("rem");
+    //Adding I format Instruction
+    If.push_back("addi");
+    If.push_back("andi");
+    If.push_back("ori");
+    If.push_back("lb");
+    If.push_back("ld");
+    If.push_back("lh");
+    If.push_back("lw");
+    If.push_back("jalr");
+    //Adding S format
+    Sf.push_back("sb");
+    Sf.push_back("sw");
+    Sf.push_back("sd");
+    Sf.push_back("sh");
+    //Addig Sb format
+
+
     
     int flag=0;
     for(int i=0;i<lines.size();i++)
     {
         string ans="";
-        string opcode="";
+        string Mnemonic="";
         int j=0;
         //Getting the opcode until space and avoiding space in from of the opcode
-        while(lines[i][j]!=' ' || opcode=="")
+        while(lines[i][j]!=' ' || Mnemonic=="")
         {
             //Checking if the instruction is comment or not
             if(lines[i][j]=='#')
@@ -53,7 +74,7 @@ int main()
                 flag++;
                 break;
             }
-            opcode=opcode+lines[i][j];
+            Mnemonic=Mnemonic+lines[i][j];
             j++;
             
         }
@@ -65,14 +86,49 @@ int main()
             continue;
         }
         //If not a comment then proceeding with instruction
+        //Checking if its R_format Mnemonics
+        
         for(int k=0;k<Rf.size();k++)
         {
-            if(opcode==Rf[k])
+            if(Mnemonic==Rf[k])
             {
-                ans=R(lines[i],j,opcode);
+                ans=R(lines[i],j,Mnemonic);
                 break;
             }
         }
+        cout<<ans<<"\n";
+        //Checking if its I format Mnemonic
+        if(ans=="")
+        {
+            for(int k=0;k<If.size();k++)
+            {
+                if(Mnemonic==If[k])
+                {
+                    ans=I(lines[i],j,Mnemonic);
+                    break;
+                }
+            
+            }
+            cout<<ans<<"\n";
+            
+ 
+        }
+        //Checking if its S format Mnemonic
+        if(ans=="")
+        {
+            for(int k=0;k<Sf.size();k++)
+            {
+                if(Mnemonic==Sf[k])
+                {
+                    ans=S(lines[i],j,Mnemonic);
+                    break;
+                }
+            
+            }
+            cout<<ans<<"\n";
+
+        }
+        //Next time I will do S Mnemonics
         
 
     }
