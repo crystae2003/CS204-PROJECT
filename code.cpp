a8e59670adae305d1c4b4a5f5b8d46f5c4209b8a
SHAIKH_ASRA_SWALEH <160813649+ammabb@users.noreply.github.com>
2024-03-05 01:50:12 +0530

    Update code.cpp
    added every format, and .data and .text attribute ans handled.asciiz data type

--- code.cpp
+++ code.cpp
@@ -4,13 +4,48 @@
 #include "R.h"
 #include "I.h"
 #include "S.h"
+#include "U.h"
+#include "SB.h"
+#include "J.h"
 using namespace std;
+int PC=0;
+int dat=10000000;
+int factor=0;
 //R format
 vector<string>Rf;
 //I format
 vector<string>If;
 //S format
 vector<string>Sf;
+//UJ format
+vector<string>Uf;
+//SB format
+vector<string>Sbf;
+//J format
+vector<string>Jf;
+//Function to determine id .data type is word or other
+void data_func(string data_type)
+{
+    if(data_type=="byte")
+    factor=1;
+    else if(data_type=="half")
+    factor=2;
+    else if(data_type=="word")
+    factor=4;
+    else 
+    factor=8;
+}
+string hexa_convert(int y)
+{
+    string an="";
+    while(y>0)
+    {
+        int r=y%16;
+        y=y/16;
+        an=char(r+'0')+an;
+    }
+    return an;
+}
 
 int main()
 {
@@ -56,17 +91,26 @@ int main()
     Sf.push_back("sd");
     Sf.push_back("sh");
     //Addig Sb format
+    Sbf.push_back("beq");
+    Sbf.push_back("bne");
+    Sbf.push_back("bge");
+    Sbf.push_back("blt");
+    //Adding U format
+    Uf.push_back("auipc");
+    Uf.push_back("lui");
+    //Adding UJ format
+    Jf.push_back("jal");
+//END of inserting Mnemonics
 
-
-    
     int flag=0;
+    ofstream output("ans.mc");
     for(int i=0;i<lines.size();i++)
     {
         string ans="";
         string Mnemonic="";
         int j=0;
         //Getting the opcode until space and avoiding space in from of the opcode
-        while(lines[i][j]!=' ' || Mnemonic=="")
+        while((lines[i][j]!=' ' || Mnemonic=="") && j!=lines[i].size())
         {
             //Checking if the instruction is comment or not
             if(lines[i][j]=='#')
@@ -74,61 +118,195 @@ int main()
                 flag++;
                 break;
             }
+            //cout<<Mnemonic<<i<<"\n";
+            //cout<<lines[i].size()<<"\n";
             Mnemonic=Mnemonic+lines[i][j];
             j++;
             
         }
-        
-        //If its a comment
-        if(flag)
+        //cout<<i<<"\n";
+        if(Mnemonic==".data:")
         {
-            flag=0;
-            continue;
-        }
-        //If not a comment then proceeding with instruction
-        //Checking if its R_format Mnemonics
-        
-        for(int k=0;k<Rf.size();k++)
-        {
-            if(Mnemonic==Rf[k])
+            //cout<<"hello\n";
+            string data_type="";
+            i++;
+            int j=0;
+            while(lines[i]!=".text:" && i!=lines.size())
             {
-                ans=R(lines[i],j,Mnemonic);
-                break;
+                  
+                   while(lines[i][j]!='.')
+                   {
+                      j++;
+                   }
+                   j++;
+                   while(lines[i][j]!=' ')
+                   {
+                      data_type+=lines[i][j];
+                      j++;
+                   }
+                  // cout<<data_type<<"\n";
+                   if(data_type=="asciiz")
+                   {
+                        //cout<<"o\n";
+                        string str="";
+                        while(lines[i][j]!='\"')
+                        {
+                            //cout<<"hello\n";
+                            j++;
+                        }
+                        //cout<<"p\n";
+                        j++;
+                        while(lines[i][j]!='\"')
+                        {
+                            int y=lines[i][j];
+                            //cout<<y<<"\n";
+                            string h=hexa_convert(y);
+                            //cout<<h<<"\n";
+                            output<<"0x"<<dat<<" 0x"<<h<<"\n";
+                            dat++;
+                            j++;
+                        }
+                        //cout<<"l\n";
+
+                   }
+                   else
+                   {
+                        data_func(data_type);
+                   }
+
+
+
+
+                i++;
             }
-        }
-        cout<<ans<<"\n";
-        //Checking if its I format Mnemonic
-        if(ans=="")
-        {
-            for(int k=0;k<If.size();k++)
-            {
-                if(Mnemonic==If[k])
-                {
-                    ans=I(lines[i],j,Mnemonic);
-                    break;
-                }
             
-            }
-            cout<<ans<<"\n";
-            
- 
-        }
-        //Checking if its S format Mnemonic
-        if(ans=="")
-        {
-            for(int k=0;k<Sf.size();k++)
-            {
-                if(Mnemonic==Sf[k])
-                {
-                    ans=S(lines[i],j,Mnemonic);
-                    break;
-                }
-            
-            }
-            cout<<ans<<"\n";
 
         }
-        //Next time I will do S Mnemonics
+        else
+        {
+            //If its a comment
+            if(flag)
+            {
+                flag=0;
+                continue;
+            }
+            //If not a comment then proceeding with instruction
+            //Checking if its R_format Mnemonics
+            if(ans=="")
+            {
+                for(int k=0;k<Rf.size();k++)
+                {
+                    if(Mnemonic==Rf[k])
+                    {
+                       ans=R(lines[i],j,Mnemonic);
+                       break;
+                    }
+                }
+                if(ans!="")
+                {
+                   output<<"0x"<<PC<<" "<<ans<<"\n";
+                   PC+=4;
+                }
+            }
+
+            //Checking if its I format Mnemonic
+            if(ans=="")
+            {
+                for(int k=0;k<If.size();k++)
+                {
+                    if(Mnemonic==If[k])
+                    {
+                        ans=I(lines[i],j,Mnemonic);
+                        break;
+                    }
+            
+                }
+                if(ans!="")
+                {
+                   output<<"0x"<<PC<<" "<<ans<<"\n";
+                   PC+=4;
+                }
+            
+ 
+            }
+            //Checking if its S format Mnemonic
+            if(ans=="")
+            {
+                for(int k=0;k<Sf.size();k++)
+                {
+                    if(Mnemonic==Sf[k])
+                    {
+                        ans=S(lines[i],j,Mnemonic);
+                        break;
+                    }
+            
+                }
+                if(ans!="")
+                {
+                   output<<"0x"<<PC<<" "<<ans<<"\n";
+                   PC+=4;
+                }
+
+            }
+            //Checking if its SB format Mnemonics
+            if(ans=="")
+            {
+                for(int k=0;k<Sbf.size();k++)
+                {
+                    if(Mnemonic==Sbf[k])
+                    {
+                        ans=S(lines[i],j,Mnemonic);
+                        break;
+                    }
+            
+                }
+                if(ans!="")
+                {
+                   output<<"0x"<<PC<<" "<<ans<<"\n";
+                   PC+=4;
+                }
+
+            }
+            //Checking if its U format Mnemonics
+            if(ans=="")
+            {
+                for(int k=0;k<Uf.size();k++)
+                {
+                    if(Mnemonic==Uf[k])
+                    {
+                        ans=U(lines[i],j,Mnemonic);
+                        break;
+                    }
+            
+                }
+                if(ans!="")
+                {
+                   output<<"0x"<<PC<<" "<<ans<<"\n";
+                   PC+=4;
+                }
+
+            }
+            //Checking if its UJ format Knemonics
+            if(ans=="")
+            {
+                for(int k=0;k<Jf.size();k++)
+                {
+                    if(Mnemonic==Jf[k])
+                    {
+                        ans=S(lines[i],j,Mnemonic);
+                        break;
+                    }
+            
+                }
+                if(ans!="")
+                {
+                   output<<"0x"<<PC<<" "<<ans<<"\n";
+                   PC+=4;
+                }
+
+            }
+            
+        }
         
 
     }
